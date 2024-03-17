c20aa356f8773aacaba60330104f9c315753169b
Vedika Mishra <151787416+vedikamishragithub@users.noreply.github.com>
2024-03-17 23:58:50 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,10 @@
 function validEmail(str) {
   //your JS code here.
+  var checkEmail = /^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$/.test(str);
+   return checkEmail; 
+	
+	
+	
 }
 
 // Do not change the code below.
